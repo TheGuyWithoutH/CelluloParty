a51c622281cf12b3954d5874dc2b43f6797d842c
TheGuyWithoutH <ugo.balducci@epfl.ch>
2022-05-12 09:09:49 +0200

    Scripts

---
+++ Assets/Scripts/Game/Cellulos/PlayerCelluloBehavior.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Game.Cellulos
+{
+    public class PlayerCelluloBehavior : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Game/Mini-Games/Quiz.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Game.Mini_Games
+{
+    public class Quiz : MonoBehaviour
+    {
+        
+    }
+}
