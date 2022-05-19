2666377c4b45990cd32b4af27902ec6f5d92feb3
TheGuyWithoutH <ugo.balducci@epfl.ch>
2022-05-19 10:39:55 +0200

    timer

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,18 +1,71 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Game;
+using Game.Cellulos;
+using Game.Mini_Games;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public CelluloPlayer player1;
+    private int _player1Tile;
+    public CelluloPlayer player2;
+    private int _player2Tile;
+
+    public Mini_Game curling;
+    public Mini_Game mole;
+    public Mini_Game quiz;
+    public Mini_Game simonSays;
+
+    public Camera mainCamera;
+    public Camera player1Camera;
+    public Camera player2Camera;
+
+    public void Awake()
+    {
+        EnableCamera(CameraView.MainCamera);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player1Tile = 0;
+        _player2Tile = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void EnableCamera(CameraView camera)
+    {
+        switch (camera)
+        {
+            case CameraView.MainCamera :
+                mainCamera.enabled = true;
+                player1Camera.enabled = false;
+                player2Camera.enabled = false;
+                break;
+            case CameraView.Player1:
+                mainCamera.enabled = false;
+                player1Camera.enabled = true;
+                player2Camera.enabled = false;
+                break;
+            case CameraView.Player2:
+                mainCamera.enabled = false;
+                player1Camera.enabled = false;
+                player2Camera.enabled = true;
+                break;
+        }
+    }
+
+    private enum CameraView
+    {
+        MainCamera,
+        Player1,
+        Player2,
     }
 }
--- Assets/Scripts/Game/Timer.cs
+++ Assets/Scripts/Game/Timer.cs
@@ -1,9 +1,67 @@
-﻿using UnityEngine;
+﻿using Game.Mini_Games;
+using UnityEngine;
+using System;
 
 namespace Game
 {
     public class Timer : MonoBehaviour
     {
+        private int _maxSeconds;
+        private float _currentTime;
+        private float _lastTime;
+        private bool _paused;
+        private bool _end;
+
+        private Mini_Game _currentMiniGame;
+
+        public void StartTimer(int seconds, Mini_Game game)
+        {
+            _maxSeconds = seconds;
+            _currentTime = 0;
+            _lastTime = Time.time;
+            _paused = false;
+
+            _currentMiniGame = game;
+        }
+        
+        public void Update()
+        {
+            if (!_paused)
+            {
+                _currentTime += Time.time - _lastTime;
+            
+                var minutes = (int)Math.Floor(_currentTime / 60);
+                var seconds = (int)Math.Floor(_currentTime % 60);
+                    
+                if (!_end && _currentTime >= _maxSeconds)
+                {
+                    _end = true;
+                }
+                else if (!_end)
+                {
+                        
+                    //timerText.SetText(string.Format("{0:00}:{1:00}", minutes, seconds));
+                }
+            }
+        
+            _lastTime = Time.time;
+
+        }
+
+        public void PauseTimer()
+        {
+            _paused = true;
+        }
+
+        public void ResumeTimer()
+        {
+            _paused = false;
+        }
+
+        private void TimerEnded()
+        {
+            _currentMiniGame.GameEnded();
+        }
         
     }
 }
