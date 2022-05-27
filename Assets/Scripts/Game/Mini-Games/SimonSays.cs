cc295e16267d18bd77e77d5686749acb20f4705f
Quentin Gross <quentin.gross@epfl.ch>
2022-05-27 20:16:26 +0200

    SimonSays

--- Assets/Scripts/Game/Mini-Games/SimonSays.cs
+++ Assets/Scripts/Game/Mini-Games/SimonSays.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Game.Mini_Games
 {
@@ -8,28 +11,97 @@ namespace Game.Mini_Games
     {
         private int _numKeys;
         private List<int> _gameKeys;
-        private List<int> _playerKeys;
-        private float latence;
-        
+        private List<int> _player1Keys;
+        private List<int> _player2Keys;
+        private float _latence;
+        private Status _status;
+        private bool _touch1;
+        private bool _touch2;
+
         protected override void Start()
         {
             base.Start();
-            MaxSeconds = 10;
         }
 
         public override void Update()
         {
             base.Update();
-            // foreach (var key in _gameKeys)
-            // {
-            //     
-            // }
+            
+            /*if (GameStatus == GameStatus.STARTED) {
+                if (_status == Status.Pattern) {
+                    GenerateKeys();
+                    _status = Status.None;
+                    for (int i = 0; i < _numKeys; i++)
+                    {
+                        ShowKey(i);
+                        if (i == _numKeys - 1)
+                        {
+                            var task7 = Task.Run(() => Status.Playing);
+                            if (task7.Wait(TimeSpan.FromSeconds(i*_latence)))
+                            {
+                                _status = task7.Result;
+                            }
+                        }
+                    }
+                }
+
+                if (_status == Status.Playing)
+                {
+                    int player1KeysCount = _player1Keys.Count;
+                    int player2KeysCount = _player2Keys.Count;
+
+                    if (player1.IsTouch && !_touch1)
+                    {
+                        _touch1 = true;
+                        int key = player1.Key;
+                        if (player1KeysCount < _numKeys) {
+                            if (_gameKeys[player1KeysCount] == key)
+                            {
+                                _player1Keys.Add(key);
+                            }
+                            else
+                            {
+                                GameEnded();
+                                player2.Score += 1;
+                            }
+                        }
+                    }
+                    _touch1 = player1.IsTouch;
+
+                    if (player2.IsTouch && !_touch2)
+                    {
+                        _touch2 = true;
+                        int key = player2.Key;
+                        if (player2KeysCount < _numKeys)
+                        {
+                            if (_gameKeys[_player2Keys.Count] == key)
+                            {
+                                _player2Keys.Add(key);
+                            }
+                            else
+                            {
+                                GameEnded();
+                                player1.Score += 1;
+                            }
+                        }
+                    }
+                    _touch2 = player2.IsTouch;
+
+                    if (player1KeysCount == _numKeys && player2KeysCount == _numKeys)
+                    {
+                        _status = Status.Pattern;
+                        _numKeys += 1;
+                        _latence -= _latence > 200 ? 200 : 0;
+                    }
+                }
+            }*/
         }
 
         public override void StartGame()
         {
             base.StartGame();
-            latence = 2000;
+            _latence = 2000;
+            _status = Status.Pattern;
         }
         
         public override void OnGamePause()
@@ -50,16 +122,24 @@ namespace Game.Mini_Games
             }
         }
 
-        private bool KeyIsValid(int key, int index)
+        private void ShowKey(int index)
         {
-            return _gameKeys[index] == key;
+            var task = Task.Run(() => true);
+            if (task.Wait(TimeSpan.FromSeconds(index*_latence)))
+            {
+                if (task.Result)
+                {
+                    player1.GetComponent<CelluloAgentRigidBody>().SetVisualEffect(VisualEffect.VisualEffectConstSingle, Color.red, _gameKeys[index]);
+                    player2.GetComponent<CelluloAgentRigidBody>().SetVisualEffect(VisualEffect.VisualEffectConstSingle, Color.red, _gameKeys[index]);
+                }
+            }
         }
-        
+
         private enum Status
         {
-            READY,
-            PATTERN,
-            PLAYING,
+            None = -1,
+            Pattern,
+            Playing,
         }
     }
 }
