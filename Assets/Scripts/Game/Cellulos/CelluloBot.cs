333791b957b5f47c515d747046512ffa37f04fd5
Kaan-wq <kaanucar9@gmail.com>
2022-06-10 19:36:53 +0200

    vas pas de rebond

--- Assets/Scripts/Game/Cellulos/CelluloBot.cs
+++ Assets/Scripts/Game/Cellulos/CelluloBot.cs
@@ -7,25 +7,5 @@ namespace Game.Cellulos
     public class CelluloBot : AgentBehaviour
     {
         public CelluloAgent celluloAgent;
-        public CelluloPlayer player_one;
-        public CelluloPlayer player_two;
-        public Curling curl;
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (curl.OkOne && collision.collider.CompareTag("Player_one"))
-            {
-                player_one.celluloAgent.SetSteering(new Steering());
-                Vector3 rebound = curl.Target + (curl.Target - curl.ThrowOne);
-                player_one.celluloAgent.SetGoalPosition(rebound.x, rebound.z, 1f);
-            }
-
-            if (curl.OkTwo && collision.collider.CompareTag("Player_two"))
-            {
-                player_two.celluloAgent.SetSteering(new Steering());
-                Vector3 rebound = curl.Target + (curl.Target - curl.ThrowTwo);;
-                player_two.celluloAgent.SetGoalPosition(rebound.x, rebound.z, 1f);
-            }
-        }
     }
 }
